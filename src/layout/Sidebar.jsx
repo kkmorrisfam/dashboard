482a31cb7ee9215d8e8a6f7b936440505a21b6ba
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-05-08 20:58:38 -0700

    start adding nav

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getNav } from '../navigation/index';
 
 const Sidebar = () => {
+
+
+    const [allNav, setAllNav] = useState([])
+    useEffect(()=>{
+        const navs = getNav('admin')
+        setAllNav(navs)
+    }, [])
+
+    console.log(allNav)
     return (
         <div>
-            <h1>Side Bar</h1>
+            <div></div>
+
+            <div className={'w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shaddow-[0_0_15px_0_rgb(34_41_47/_5%)] transition-all'}>
+                <div className='h-[70px] flex justify-center items-center'>
+                    <Link to='/' className='w-[180px] h-[50px]'>
+                        <img className='w-full h-full' src='http://localhost:3000/images/logo.png' alt="Easy Shop logo"/>
+                    </Link>
+                </div>
+                <div className='px-[16px]'>
+                    
+                </div>
+
+            </div>
         </div>
     );
 };
