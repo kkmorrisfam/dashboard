b9bac4b460c392d292171d43d408a33352f45087
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-05-12 18:46:35 -0700

    Add test admin dashboard

--- src/views/admin/AdminDashboard.jsx
+++ src/views/admin/AdminDashboard.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const AdminDashboard = () => {
     return (
-        <div>
-            <h1>Admin Dashboard</h1>
+        <div className='px-2 md:px-7 py-5'>
+            <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7'>
+                
+                <div className='flex justify-between items-center p-5 bg-[#fae8e8] rounded-md gap-3'>
+                    <div className='flex flex-col justify-start items-start text-[#5c5a5a]'>
+                        <h2>$3434</h2>
+                        <span>Total Sale</span>
+                    </div>
+
+                </div>
+
+            </div>
+            
         </div>
     );
 };
